1da0950430afc39fabf7b0bdf6289f662cfa3641
chirag <chiragsinghal04@gmail.com>
2024-03-18 23:58:26 +0530

    update

--- src/controllers/jobs.controller.js
+++ src/controllers/jobs.controller.js
@@ -2,13 +2,13 @@ import jobModel from "../models/jobs.model.js";
 class JobController{
     getJobs(req,res){
         var jobs = jobModel.getAll();
-        res.render('jobs',{jobs});
+        res.render('jobs',{jobs,userEmail:req.session.userEmail});
     }
     getJobDetail(req,res){
         const id= req.params.id;
         const jobFound= jobModel.getByID(id);
         if(jobFound){
-            res.render('jobDetail',{job:jobFound});
+            res.render('jobDetail',{job:jobFound,userEmail:req.session.userEmail});
         }else{
             res.status(401).send('job not found');
         }
@@ -17,13 +17,13 @@ class JobController{
         res.render('login');
     }
     getPostjob(req,res){
-        res.render('post-job');
+        res.render('post-job',{userEmail:req.session.userEmail});
     }
     postPostjob(req,res){
         const {jobCategory,jobDesignation,jobLocation,companyName,salary,applyby,skillsReq,noofopenings,jobposted,applicants} = req.body;
             jobModel.add(jobCategory,jobDesignation,jobLocation,companyName,salary,applyby,skillsReq,noofopenings,jobposted,applicants);
             var jobs=jobModel.getAll();
-            res.render('jobs',{jobs});
+            res.render('jobs',{jobs,userEmail:req.session.userEmail});
     }
     
 }
